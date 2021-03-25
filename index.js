318eace73ff1797288be19b93a70588e3cf73079
Justin <jmp86@yahoo.com>
2021-03-24 14:48:38 -1000

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const name = [];
+
+function writeCards(array, surprise) {
+  for (let i = 0; i < array.length; i++) {
+    debugger;
+    name.push(`Thank you, ${array[i]}, for the wonderful ${surprise} gift!`);
+   
+  }
+  return name;
+}
+
+
+function countDown(number) { 
+    for (let count = 10; count >= 0; count--) { 
+        console.log(count); 
+    }
+}
